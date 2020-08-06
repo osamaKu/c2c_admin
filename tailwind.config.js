eb43bd8b79d2348b1f39a367b1a80deba22f64b1
osamaKu <61397012+osamaKu@users.noreply.github.com>
2020-08-06 11:43:39 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: {
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
